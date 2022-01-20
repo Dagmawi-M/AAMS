0f150c12ebb84b2028e8674d8ac5edb5ce4bf652
Dagmawi-M <43979542+Dagmawi-M@users.noreply.github.com>
2022-01-20 19:34:25 +0300

    Register and login related. Models , Dbentity , Views

---
+++ AAMS/Models/DBentities.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace AAMS.Models
+{
+    public class DBentities : DbContext
+    {
+        public DBentities() : base("DatabaseMVC5") { }
+        public DbSet<User> Users { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //Database.SetInitializer<demoEntities>(null);
+            modelBuilder.Entity<User>().ToTable("Users");
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
