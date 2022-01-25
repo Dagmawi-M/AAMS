d0810c91645664ff92b36c9e5e49096e39eafc67
Elroe Gebreab <elroetadele292@gmail.com>
2022-01-25 23:11:35 +0300

    added attendance sheet controller

---
+++ AAMS/Migrations/202201251749018_second-migration.cs
@@ -0,0 +1,34 @@
+﻿namespace AAMS.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class secondmigration : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Users",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        FirstName = c.String(nullable: false, maxLength: 50),
+                        FatherName = c.String(nullable: false, maxLength: 50),
+                        GrandFatherName = c.String(nullable: false, maxLength: 50),
+                        Email = c.String(nullable: false),
+                        Password = c.String(nullable: false),
+                        Role = c.String(),
+                        StudentCode = c.String(),
+                        Batch = c.String(),
+                        Discriminator = c.String(nullable: false, maxLength: 128),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Users");
+        }
+    }
+}
