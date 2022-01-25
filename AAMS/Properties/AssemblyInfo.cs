166a9774163572d24296879eb2821a732fd5bfd2
Dagmawi-M <43979542+Dagmawi-M@users.noreply.github.com>
2022-01-25 08:20:59 +0300

    fresh project

--- AAMS/Properties/AssemblyInfo.cs
+++ AAMS/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("2c032b7f-9eec-4b88-924b-85cdb179a642")]
+[assembly: Guid("164587d7-1142-4042-902e-94aab7c4a0ad")]
 
 // Version information for an assembly consists of the following four values:
 //
