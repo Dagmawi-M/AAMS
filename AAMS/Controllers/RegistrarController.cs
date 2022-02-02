56791493059621f26410a60e76af6c609075f98e
Dagmawi-M <43979542+Dagmawi-M@users.noreply.github.com>
2022-02-02 17:40:33 +0300

    Minor changes to student and registrar controller, and DASHBOARD

--- AAMS/Controllers/RegistrarController.cs
+++ AAMS/Controllers/RegistrarController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AAMS.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,12 +7,19 @@ using System.Web.Mvc;
 
 namespace AAMS.Controllers
 {
+
+
     public class RegistrarController : Controller
     {
+        ApplicationDbContext _context = new ApplicationDbContext();
         // GET: Registrar
         public ActionResult Index()
         {
-            return View();
+           // var uniqueSheets = _context.AttendanceSheets.Select(a => new { a.CourseId, a.Courses, a.Section }).Distinct().ToList();
+            //ViewBag.unique = uniqueSheets;
+           // return View(_context.AttendanceSheets.GroupBy(a => a.CourseId).Select(a => a.FirstOrDefault()).ToList());
+            var course = _context.Courses.ToList();
+            return View(course);
         }
     }
 }
